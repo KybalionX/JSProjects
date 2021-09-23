1b8ac2c254ac77b4e76307f4f26b20417581f83b
Nicolas <nicolaseduardoflorez1@gmail.com>
2021-09-23 10:27:41 -0500

    Proyecto 'Calculadora' updated

--- Calculadora/main.js
+++ Calculadora/main.js
@@ -2,19 +2,23 @@ let buttons = document.querySelectorAll("button");
 let input = document.querySelector("input");
 
 buttons.forEach(button => {
-    button.addEventListener("click", () =>{
+    button.addEventListener("click", () => {
         let number = button.textContent;
-        if(number == 'C'){
+        if (number == 'C') {
             input.value = "";
             return;
         }
-        if(number == '='){
-            if(input.value == ""){
+        if (number == '=') {
+            if (input.value == "") {
                 input.placeholder = "Digite algo!";
             }
             console.log(eval(input.value));
             return;
         }
-        input.value+=number;
+        if (number == '<') {
+            input.value = input.value.slice(0,-1);
+            return;
+        }
+        input.value += number;
     });
 });
