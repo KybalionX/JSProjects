0b6605fd35678721cf72d127794b653be9dcb706
Nicolas <nicolaseduardoflorez1@gmail.com>
2021-09-23 11:27:32 -0500

    Proyecto 'Counter' updated

--- Counter/main.js
+++ Counter/main.js
@@ -1,8 +1,17 @@
 let count = document.querySelector("h1");
-counter = 0;
+let counter = 0;
+let msg = document.querySelector('p');
 window.addEventListener("keyup", (event) => {
-    if(event.code == 'Space'){
+    if (event.code == 'Space') {
         counter++;
     }
-    count.innerHTML = counter
+    count.innerHTML = counter;
 });
+
+if (/Android|webOS|iPhone|iPad|iPod|BlackBerry|IEMobile|Opera Mini/i.test(navigator.userAgent)) {
+    msg.innerHTML = "Touch screen"
+    window.addEventListener('touchend', () => {
+        counter++;
+        count.innerHTML = counter;
+    });
+}
